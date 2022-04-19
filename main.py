81ac1754182052f5fc8cf3f1c45742303f904adc
NNemet <nemet.norbi.1@gmail.com>
2022-04-19 14:47:17 +0200

    First commit

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import datetime
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def hello_xy(name: str = "hthdhrehd") -> str:
+    print("#"*50)
+    if name == "hthdhrehd":
+        name = input("What is your name?")
+    else:
+        print("I know you")
+    return f"{datetime.datetime.now()} - Hello {name}!"
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(hello_xy())
+print(hello_xy("gdfgfd"))
+print(hello_xy(name="fghdgfd"))
